cfbdc3c3f729b953a3c291efbfe8993f07407386
LearnMoreTraining <anaravinthh@outlook.com>
2024-10-19 20:36:33 +0530

    added collections code

---
+++ August8PmISTIntelli/src/test/java/javabasics/ListExample.java
@@ -0,0 +1,27 @@
+package javabasics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+
+    public static void main(String[] args) {
+
+        List<Integer> l = new ArrayList<Integer>();
+        l.add(20);
+        l.add(56);
+        l.add(100);
+
+        System.out.println(l);
+        l.add(78);
+        l.add(90);
+        l.add(90);
+        l.add(90);
+        System.out.println(l);
+        System.out.println(l.get(2));
+
+        List <String> l2 = new ArrayList<String>();
+        l2.add("abc");
+
+    }
+}
--- August8PmISTIntelli/src/test/java/runner/Runner.java
+++ August8PmISTIntelli/src/test/java/runner/Runner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/test/resources/testdesgin"},
         glue = {"stepdefination"},
-        tags = "@jquery01"
+        tags = "@amazon03"
 )
 
 public class Runner {
